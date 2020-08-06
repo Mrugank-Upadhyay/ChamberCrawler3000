1ee12be13e7ff15f52d79453f211e733ea2041f6
Mrugank <mrugank2@gmail.com>
2020-08-06 18:08:15 -0400

    added potion methods

---
+++ potion.cc
@@ -0,0 +1,25 @@
+#include "player.h"
+#include "potion.h"
+
+Potion::Potion(std::string type, std::string rep, std::pair<int, int> position)
+    : Item{type, rep, position} {}
+
+void Potion::apply(Player p) {
+    p->applyItem(std::make_shared<Potion>(*this));
+}
+
+std::string Potion::printInfo() {
+    std::string message;
+
+    if (health != 0) {
+        message = std::to_string(health);
+    }
+
+    else if (atk != 0) {
+        message = std::to_string(atk);
+    }
+
+    else if (def != 0) {
+        message = std::to_string(def);
+    } 
+}
