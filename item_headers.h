d0c637964a4093b4bee8b2e31c4c7df712995240
Mrugank <mrugank2@gmail.com>
2020-08-07 18:55:01 -0400

    fixed #include

--- item_headers.h
+++ item_headers.h
@@ -2,7 +2,7 @@
 #define _ITEM_HEADERS_
 
 #include "item_potion_headers.h"
-#include "items/gold.h"
-#include "items/potion.h"
+#include "./items/gold.h"
+#include "./items/potion.h"
 
 #endif
--- item_potion_headers.h
+++ item_potion_headers.h
@@ -1,11 +1,11 @@
 #ifndef _ITEM_POTION_HEADERS_
 #define _ITEM_POTION_HEADERS_
 
-#include "../items/potion_types/RHpotion.h"
-#include "../items/potion_types/BApotion.h"
-#include "../items/potion_types/BDpotion.h"
-#include "../items/potion_types/PHpotion.h"
-#include "../items/potion_types/WApotion.h"
-#include "../items/potion_types/WDpotion.h"
+#include "./items/potion_types/RHpotion.h"
+#include "./items/potion_types/BApotion.h"
+#include "./items/potion_types/BDpotion.h"
+#include "./items/potion_types/PHpotion.h"
+#include "./items/potion_types/WApotion.h"
+#include "./items/potion_types/WDpotion.h"
 
 #endif
