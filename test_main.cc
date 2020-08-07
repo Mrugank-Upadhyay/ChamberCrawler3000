54fda09a3c70687540b78da166c5ec5c0a8f123e
Mrugank <mrugank2@gmail.com>
2020-08-06 21:08:35 -0400

    printInfo function in progress for testing

--- test_main.cc
+++ test_main.cc
@@ -1,8 +1,20 @@
 #include "headerfiles.h"
 #include <iostream>
+using namespace std;
 
+void printInfo(std::shared_ptr<Character> character) {
+    cout << character->getHP() << " "
+         << character->getATK() << " "
+         << character->getDEF() << " "
+         << character->getRace() << " "
+         << character->getRep() << " ";
+    if (typeid(character) == typeid(Player)) {
+        std::shared_ptr<Player> characterP = dynamic_pointer_cast<Player>(character);
+        cout << characterP->getGold();
+    }
+}
 
 int main() {
     std::shared_ptr<Human> human = std::make_shared<Human>(std::pair<int, int>(0,0));
-    
+    cout << human->getHP();
 }
