a3e200e6894dcb6d75fb13ccc25a479aaa04fc52
Mrugank <mrugank2@gmail.com>
2020-08-06 20:28:36 -0400

    implemented Shade

---
+++ player_races/shade.cc
@@ -0,0 +1,8 @@
+#include "shade.h"
+
+Shade::Shade(std::pair<int, int> position)
+    : Player{"Shade", "@", 125, 25, 25, position, 0, 125, 25, 25, false} {}
+
+void Shade::attack(std::shared_ptr<Enemy> enemy) {
+    enemy->getStruckBy(std::make_shared<Shade>(*this));
+}
--- player_races/shade.h
+++ player_races/shade.h
@@ -1,5 +1,12 @@
+#ifndef _SHADE_
+#define _SHADE_
+
 #include "../player.h"
 
 class Shade : public Player {
-    
+    public:
+        Shade(std::pair<int, int> position);
+        void attack(std::shared_ptr<Enemy> enemy) override;
 };
+
+#endif
