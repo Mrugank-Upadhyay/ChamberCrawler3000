09411473eb048463c9f698411221006bbcf4d001
Mrugank <mrugank2@gmail.com>
2020-08-07 18:53:59 -0400

    fixed #include

--- player_headers.h
+++ player_headers.h
@@ -1,10 +1,10 @@
 #ifndef _PLAYER_HEADERS_
 #define _PLAYER_HEADERS_
 
-#include "../player_races/shade.h"
-#include "../player_races/drow.h"
-#include "../player_races/vampire.h"
-#include "../player_races/troll.h"
-#include "../player_races/goblin.h"
+#include "./player_races/shade.h"
+#include "./player_races/drow.h"
+#include "./player_races/vampire.h"
+#include "./player_races/troll.h"
+#include "./player_races/goblin.h"
 
 #endif
