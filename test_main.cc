92658fa3ffe52f3bce89f515e4cc7e72cff43afa
Mrugank <mrugank2@gmail.com>
2020-08-07 17:50:08 -0400

    completed printInfo function for testing Character

--- test_main.cc
+++ test_main.cc
@@ -7,14 +7,25 @@ void printInfo(std::shared_ptr<Character> character) {
          << character->getATK() << " "
          << character->getDEF() << " "
          << character->getRace() << " "
-         << character->getRep() << " ";
+         << character->getRep() << " "
+         << character->getPosition().first << "," << character->getPosition().second << " ";
     if (typeid(character) == typeid(Player)) {
         std::shared_ptr<Player> characterP = dynamic_pointer_cast<Player>(character);
-        cout << characterP->getGold();
+        cout << characterP->getGold() << " "
+             << characterP->getMaxHP() << " "
+             << characterP->getTmpATK() << " "
+             << characterP->getTmpDEF() << " "
+             << boolalpha << characterP->isTurnFinished() << " ";
     }
+
+    else {
+        std::shared_ptr<Enemy> characterE = dynamic_pointer_cast<Enemy>(character);
+        cout << boolalpha << characterE->getHostile() << " "
+             << characterE->getGold() << " ";
+    }   
 }
 
 int main() {
     std::shared_ptr<Human> human = std::make_shared<Human>(std::pair<int, int>(0,0));
-    cout << human->getHP();
+    printInfo(human);
 }
