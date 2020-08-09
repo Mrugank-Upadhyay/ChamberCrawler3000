f73187d823b2489b9458a9a7851cda0d4e3541bf
Mrugank <mrugank2@gmail.com>
2020-08-08 21:28:54 -0400

    changed headers

--- enemy_headers.h
+++ enemy_headers.h
@@ -1,12 +1,12 @@
 #ifndef _ENEMY_HEADERS_
 #define _ENEMY_HEADERS_
 
-#include "enemy_races/human.h"
-#include "enemy_races/dwarf.h"
-#include "enemy_races/elf.h"
-#include "enemy_races/orc.h"
-#include "enemy_races/merchant.h"
-#include "enemy_races/dragon.h"
-#include "enemy_races/halfling.h"
+#include "./enemy_races/human.h"
+#include "./enemy_races/dwarf.h"
+#include "./enemy_races/elf.h"
+#include "./enemy_races/orc.h"
+#include "./enemy_races/merchant.h"
+#include "./enemy_races/dragon.h"
+#include "./enemy_races/halfling.h"
 
 #endif
