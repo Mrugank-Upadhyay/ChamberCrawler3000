b3ad79b9239b4ef67fb3b2d4a5b40bc6db1c1c0b
Mrugank <mrugank2@gmail.com>
2020-08-08 21:07:47 -0400

    added floor class

---
+++ floor.h
@@ -0,0 +1,22 @@
+#include <map>
+#include <vector>
+#include <memory>
+
+class Cell;
+class Enemy;
+class Gold;
+class Potion;
+
+
+class Floor {
+    std::map<std::pair<int, int>, std::shared_ptr<Cell>> grid;
+    std::vector<std::shared_ptr<Enemy>> enemies;
+    std::vector<std::shared_ptr<Potion>> potions;
+    std::vector<std::shared_ptr<Gold>> goldPiles;
+    std::shared_ptr<Cell> exit;
+
+    public:
+        Floor(); 
+
+
+};
