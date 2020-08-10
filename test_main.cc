a545c6d6fed24c9385819376ebbdd2567867fc91
Mihran Mashhud <mihranmashhud@gmail.com>
2020-08-10 18:48:40 -0400

    testing testing

--- test_main.cc
+++ test_main.cc
@@ -24,35 +24,14 @@
 #include <iostream>
 using namespace std;
 
-//void printInfo(std::shared_ptr<Character> character) {
-    //cout << character->getHP() << " "
-         //<< character->getATK() << " "
-         //<< character->getDEF() << " "
-         //<< character->getRace() << " "
-         //<< character->getRep() << " "
-         //<< character->getPosition().first << "," << character->getPosition().second << " ";
-    //if (typeid(character) == typeid(Player)) {
-        //std::shared_ptr<Player> characterP = dynamic_pointer_cast<Player>(character);
-        //cout << characterP->getGold() << " "
-             //<< characterP->getMaxHP() << " "
-             //<< characterP->getTmpATK() << " "
-             //<< characterP->getTmpDEF() << " "
-             //<< boolalpha << characterP->isTurnFinished() << " " << endl;
-    //}
-
-    //else {
-        //std::shared_ptr<Enemy> characterE = dynamic_pointer_cast<Enemy>(character);
-        ////cout << boolalpha << characterE->getHostile() << " " << endl;
-             ////<< characterE->getGold() << " " << endl;
-    //}   
-//}
-
 int main() {
     auto human = std::make_shared<Human>(std::pair<int, int>(0,0)); // Enemy
     auto troll = std::make_shared<Troll>(std::pair<int, int>(1,4)); // Player
     auto bapotion = std::make_shared<BAPotion>("?", std::pair<int,int>(9,8)); // Potion
     auto goldpile = std::make_shared<Gold>(std::make_pair<int,int>(1,1),6); // Gold
 
+    human->attack(troll);
+    troll->attack(human);
     cout << human->info() << endl;
     cout << troll->info() << endl;
     cout << bapotion->info() << endl;
