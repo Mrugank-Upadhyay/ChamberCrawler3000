f91c43144a49e82d9099a02d4d97be2f7a23d91b
Mihran Mashhud <mihranmashhud@gmail.com>
2020-08-10 01:10:20 -0400

    testing out new info function

--- test_main.cc
+++ test_main.cc
@@ -2,30 +2,44 @@
 #include <iostream>
 using namespace std;
 
-void printInfo(std::shared_ptr<Character> character) {
-    cout << character->getHP() << " "
-         << character->getATK() << " "
-         << character->getDEF() << " "
-         << character->getRace() << " "
-         << character->getRep() << " "
-         << character->getPosition().first << "," << character->getPosition().second << " ";
-    if (typeid(character) == typeid(Player)) {
-        std::shared_ptr<Player> characterP = dynamic_pointer_cast<Player>(character);
-        cout << characterP->getGold() << " "
-             << characterP->getMaxHP() << " "
-             << characterP->getTmpATK() << " "
-             << characterP->getTmpDEF() << " "
-             << boolalpha << characterP->isTurnFinished() << " ";
-    }
+//void printInfo(std::shared_ptr<Character> character) {
+    //cout << character->getHP() << " "
+         //<< character->getATK() << " "
+         //<< character->getDEF() << " "
+         //<< character->getRace() << " "
+         //<< character->getRep() << " "
+         //<< character->getPosition().first << "," << character->getPosition().second << " ";
+    //if (typeid(character) == typeid(Player)) {
+        //std::shared_ptr<Player> characterP = dynamic_pointer_cast<Player>(character);
+        //cout << characterP->getGold() << " "
+             //<< characterP->getMaxHP() << " "
+             //<< characterP->getTmpATK() << " "
+             //<< characterP->getTmpDEF() << " "
+             //<< boolalpha << characterP->isTurnFinished() << " " << endl;
+    //}
 
-    else {
-        std::shared_ptr<Enemy> characterE = dynamic_pointer_cast<Enemy>(character);
-        cout << boolalpha << characterE->getHostile() << " "
-             << characterE->getGold() << " ";
-    }   
-}
+    //else {
+        //std::shared_ptr<Enemy> characterE = dynamic_pointer_cast<Enemy>(character);
+        ////cout << boolalpha << characterE->getHostile() << " " << endl;
+             ////<< characterE->getGold() << " " << endl;
+    //}   
+//}
 
 int main() {
-    std::shared_ptr<Human> human = std::make_shared<Human>(std::pair<int, int>(0,0));
-    printInfo(human);
+    auto human = std::make_shared<Human>(std::pair<int, int>(0,0)); // Enemy
+    auto troll = std::make_shared<Troll>(std::pair<int, int>(1,4)); // Player
+    auto bapotion = std::make_shared<BAPotion>("?", std::pair<int,int>(9,8)); // Potion
+    auto goldpile = std::make_shared<Gold>(std::make_pair<int,int>(1,1),6); // Gold
+
+    cout << human->info() << endl;
+    cout << troll->info() << endl;
+    cout << bapotion->info() << endl;
+    cout << goldpile->info() << endl;
+    
+    //printInfo(human);
+    //printInfo(troll);
+    //human->attack(troll);
+    //troll->attack(human);
+    //printInfo(human);
+    //printInfo(troll);
 }
