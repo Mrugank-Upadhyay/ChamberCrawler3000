18ac43a3d4581f7bb0994e9fd43bd0cedbebc0c8
Mihran Mashhud <mihranmashhud@gmail.com>
2020-08-12 23:46:45 -0400

    gold can be picked up (or not)

--- src/items/gold.cc
+++ src/items/gold.cc
@@ -2,10 +2,14 @@
 #include "gold.h"
 
 Gold::Gold(std::pair<int, int> position, int amount)
-    : Item{"Gold", "G", position}, amount{amount} {}
+    : Item{"Gold", "G", position}, amount{amount}, canPickUp{true} {}
 
 int Gold::getAmount() {
     return amount;
+}
+
+bool getCanPickUp() {
+    return canPickUp;
 }
 
 void Gold::apply(std::shared_ptr<Player> player) {
--- src/items/gold.h
+++ src/items/gold.h
@@ -7,10 +7,15 @@ class Player;
 
 class Gold : public Item {
     int amount;
+    bool canPickUp;
 
     public:
         Gold(std::pair<int, int> position, int amount);
         int getAmount();
+
+        bool getCanPickUp();
+        void setCanPickUp(bool canPickUp);
+
         virtual void apply(std::shared_ptr<Player> player) override;
         virtual std::string info() override;
 };
