6de3c4c49e5a195e49d199362b156e087e765d1f
Mihran Mashhud <mihranmashhud@gmail.com>
2020-08-12 21:02:51 -0400

    dragon knows how to move now

--- src/enemy_races/dragon.cc
+++ src/enemy_races/dragon.cc
@@ -1,5 +1,6 @@
 #include "dragon.h"
 #include "../player.h"
+#include "../cell.h"
 
 // If attacked, make all future Dragons hostile!
 Dragon::Dragon(std::pair<int, int> position, std::shared_ptr<Cell> goldCell)
@@ -15,7 +16,26 @@ std::shared_ptr<Cell> Dragon::getGoldCell() {
 std::pair<int, int> Dragon::move() {
   auto previous = getPosition();
 
-  
+  auto goldNeighours = goldCell->getObservers();
+  auto neighbours = getCell()->getObservers();
+  std::vector<std::shared_ptr<Cell>> intersect;
+  for(auto obs1: goldNeighours) {
+    for(auto obs2: neighbours) {
+      if(obs1 == obs2) {
+        auto obsCell =
+          std::dynamic_pointer_cast<Cell>(obs1);
+        if(!obsCell->getOccupied()) {
+          intersect.push_back(obsCell);
+        }
+      }
+    }
+  }
+
+  int len = intersect.size();
+  if(len != 0) {
+    auto dest = intersect[rand() % len];
+    getCell()->moveCharacter(dest);
+  }
 
   return previous;
 }
